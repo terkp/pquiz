a7afd43f0bef834e68db8a65149c21e73ae2d32c
ruossn1 <nilssimon.ruoss@gmail.com>
2021-10-23 17:56:51 +0200

    First ui

---
+++ lib/ui/GuessWidget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:pfingst_freizeit_quizz/model/questions/Guess.dart';
+import 'package:pfingst_freizeit_quizz/model/questions/Standard.dart';
+
+class GuessWidget extends StatefulWidget {
+  final Guess question;
+
+  const GuessWidget({Key? key, required this.question}) : super(key: key);
+
+  @override
+  State<GuessWidget> createState() => _GuessWidgetState();
+}
+
+class _GuessWidgetState extends State<GuessWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    return Column(children: [
+      Text(widget.question.title),
+      TextField(
+        controller: textController,
+        keyboardType: TextInputType.number,
+      ),
+      TextButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+            final answer = textController.value.text;
+            final guess = int.tryParse(answer);
+            if (guess != null) {
+              //Todo senden
+              print(guess);
+              if(guess == 42){
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Immer eine gute Wahl ;)"),
+                  ),
+                );
+              }
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(answer.contains(".") || answer.contains(",")
+                      ? "Nur ganze Zahlen erlaubt!"
+                      : "Bitte erst gültige Antwort eingeben"),
+                ),
+              );
+            }
+          },
+          child: Text("Absenden"))
+    ]);
+  }
+}
--- lib/ui/OrderWidget.dart
+++ lib/ui/OrderWidget.dart
@@ -1,38 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:pfingst_freizeit_quizz/model/questions/Order.dart';
 import 'package:pfingst_freizeit_quizz/model/questions/Standard.dart';
 
-class StandardWidget extends StatefulWidget {
-  final Standard question;
+class OrderWidget extends StatefulWidget {
+  final Order question;
 
-  const StandardWidget({Key? key, required this.question}) : super(key: key);
+  const OrderWidget({Key? key, required this.question}) : super(key: key);
 
   @override
-  State<StandardWidget> createState() => _StandardWidgetState();
+  State<OrderWidget> createState() => _OrderWidgetState();
 }
 
-class _StandardWidgetState extends State<StandardWidget> {
+class _OrderWidgetState extends State<OrderWidget> {
   String? currentAnswer;
 
   @override
   Widget build(BuildContext context) {
+    List<String> _items = widget.question.answers;
     return Column(
       children: [
         Text(widget.question.title),
-      ]
-        ..addAll(getAnswerRadibuttons(widget.question.answers))
-        ..add(TextButton(
-            onPressed: () {
-              if (currentAnswer == null) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text("Bitte erst Antwort auswählen"),
-                  ),
-                );
-              }else {
-                //Todo currentAnswerSenden
-              }
+        SizedBox(
+          height: _items.length*60,
+          child: ReorderableListView(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            children: <Widget>[
+              for (int index = 0; index < _items.length; index++)
+                ListTile(
+                  key: Key('$index'),
+                  trailing: Icon(Icons.view_headline),
+                  title: Text('Item ${_items[index]}'),
+                ),
+            ],
+            onReorder: (int oldIndex, int newIndex) {
+              setState(() {
+                if (oldIndex < newIndex) {
+                  newIndex -= 1;
+                }
+                final String item = _items.removeAt(oldIndex);
+                _items.insert(newIndex, item);
+              });
             },
-            child: Text("Absenden"))),
+          ),
+        ),
+        TextButton(
+          child: Text("Absenden"),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (_) => AlertDialog(
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Abbrechen"),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      //Todo absenden
+                    },
+                    child: Text("Ok"),
+                  )
+                ],
+              ),
+            );
+          },
+        )
+      ],
     );
   }
 
